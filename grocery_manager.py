7d041e8f975433250cac89ebbfdfcf7388cd7075
Noah Adams <nhadams10@uchicago.edu>
2021-09-19 04:15:11 +0000

    Wrote while loop and generalized inputs but got stuck on += functionality for Recipe class

--- grocery_manager.py
+++ grocery_manager.py
@@ -1,9 +1,6 @@
 from cookbook import chickpea_chili
 from cookbook import apple_crumb_cake
 
-#recipe = input("What do you want to cook? ")
-#print(recipe + " Is this correct?")
-
 class Recipe(dict):
     def __add__(self, oth):
         r = self.copy()
@@ -19,8 +16,42 @@ class Recipe(dict):
 
         return r
 
+grocery_dict = Recipe({})
+
 a = Recipe(apple_crumb_cake)
 b = Recipe(chickpea_chili)
-c = a + b
 
-print(c)
+print(
+    '''
+    Enter the letter for your desired recipe
+    a - apple crumb cake
+    b - chickpea chili
+    x - exit
+    '''
+)
+
+while True:
+    recipe = input("What do you want to cook?")
+    if recipe == "x":
+        break
+    elif recipe == "a":
+        grocery_dict += a
+    elif recipe == "b":
+        grocery_dict += b
+    else:
+        print("Please enter a valid recipe or x to exit")
+
+
+#print(recipe + " Is this correct?")
+#this should be updated to confirm what the user wants to cook
+
+#grocery_dict = a + b
+# this should be updated to responsively create a dictionary for all the needed groceries
+
+grocery_list = '''
+6 Apples
+5 Oranges
+'''
+#this should be updated to convert output from a dictionary to a user-friendly list
+
+print(grocery_dict)
